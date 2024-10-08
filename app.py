7f3308ba0941940f1d7e5e8ce37897c2c781e6a9
masudasatoki <bananaboyfriend123@gmail.com>
2024-10-09 00:11:18 +0900

    見かけを更新

--- app.py
+++ app.py
@@ -19,19 +19,19 @@ st.set_page_config(**const.SET_PAGE_CONFIG)
 os.chdir(os.path.dirname(__file__))
 
 # CSVファイルを読み込む。
-df3_path = 'sample_data/3個人票.csv'
+df3_path = 'data/3個人票.csv'
 # df1
-df1 = pd.read_csv("sample_data/1世帯情報.csv", encoding='utf-8')
+df1 = pd.read_csv("data/1世帯情報.csv", encoding='utf-8')
 #df2
 dtype_str= ['7_■1_現住所_住所（番地・番）', '8_■1_現住所_住所（号）', '勤務先・通学先・通園先の所在地_目標物', '勤務先・通学先・通園先の所在地_番地・番', '勤務先・通学先・通園先の所在地_号']
 dtype_dict = {i: str for i in dtype_str}
-df2 = pd.read_csv("sample_data/2世帯個人.csv", encoding='utf-8', dtype=dtype_dict)
+df2 = pd.read_csv("data/2世帯個人.csv", encoding='utf-8', dtype=dtype_dict)
 #df3
 dtype_str= ['46_出発地_目標物', '47_出発地_番地・番', '48_出発地_号', '51_到着地_目標物', '52_到着地_番地・番', '53_到着地_号']
 dtype_dict = {i: str for i in dtype_str}
 df3 = pd.read_csv(df3_path, encoding='utf-8', dtype=dtype_dict)
 # GeoJSONファイル
-geojson_file_path = 'sample_data/大ゾーン.geojson'
+geojson_file_path = 'data/大ゾーン.geojson'
 geo_data = gpd.read_file(geojson_file_path)
 
 # 目的コード
@@ -44,15 +44,15 @@ mode_dict = {1: "徒歩", 2: "自転車", 3: "原動付自転車", 4: "自動二
              11: "路線バス", 12: "鉄道（JR）", 13: "市内電車", 14: "郊外電車", 15: "船舶", 
              16: "航空機", 17: "その他", 18: "自動車（車種不明）", 99: "無回答"}
 
-zone_dict = {'松山市1区':'市駅、大街道、松山城', '松山市2区':'大手町、本町', '松山市3区':'松山大・愛媛大・松山東高', '松山市4区':'いよ立花駅', '松山市5区':'市駅南側、JR西側',
-             '松山市6区':'衣山', '松山市7区':'木屋町、山越', '松山市8区':'湯の山、北東山地', '松山市9区':'平井、梅本（東久米）', '松山市10区':'桑原・畑寺', 
-             '松山市11区':'久米', '松山市12区':'恵原、南部山地', '松山市13区':'古川、石井、いよ立花の南', '松山市14区':'市坪、松山JCT', '松山市15区':'垣生、余戸、土居田',
-             '松山市16区':'北斎院、空港線', '松山市17区':'三津浜駅、山西駅', '松山市18区':'久万ノ台、北部山地', '松山市19区':'伊予和気、堀江', '松山市20区':'興居島',
+zone_dict = {'松山市1区':'市駅、大街道、松山城', '松山市2区':'大手町、本町、味酒', '松山市3区':'愛媛大、松山東高、祝谷', '松山市4区':'いよ立花駅、朝生田', '松山市5区':'土橋、空港通、南江戸',
+             '松山市6区':'衣山、宮田町', '松山市7区':'木屋町、山越、姫原', '松山市8区':'湯の山、北東山地', '松山市9区':'平井、梅本、東久米', '松山市10区':'桑原、畑寺、東野', 
+             '松山市11区':'久米、南土居、高井', '松山市12区':'久谷、荏原、浄瑠璃', '松山市13区':'和泉南、古川北、石井、朝生田', '松山市14区':'市坪、松山IC、森松', '松山市15区':'垣生、余戸、土居田',
+             '松山市16区':'北斎院、南斎院、北吉田', '松山市17区':'JR三津浜駅、山西、松ノ木', '松山市18区':'久万ノ台、谷町、北部山地', '松山市19区':'伊予和気、堀江', '松山市20区':'高浜、興居島',
              '松山市21区':'空港', '松山市22区':'道後温泉、石手寺', '松山市23区':'JR松山駅', '松山市24区':'三津埠頭', '松山市25区':'松山観光港',
-             '松山市26区':'中央卸売市場', '松山市27区':'伊予北条', '松山市28区':'中島',
+             '松山市26区':'中央卸売市場', '松山市27区':'北条', '松山市28区':'中島',
              '伊予市1区':'郡中、伊予市駅', '伊予市2区':'伊予南部山地', '伊予市3区':'伊予西部山地',
              '東温市1区':'横河原駅、愛大医学部', '東温市2区':'東温市東部山地', '松前町':'松前町',
-             '砥部町1区':'とべ動物園', '砥部町2区':'砥部南部山地'}
+             '砥部町1区':'砥部町中心部、動物園', '砥部町2区':'砥部南部山地'}
 
 
 # タイトルと説明を追加
@@ -72,7 +72,7 @@ with col1:
     style_function = lambda x: {
         'fillColor': '#transparent',
         'color': '#000000',
-        'weight': 1.5,
+        'weight': 1.0,
         'fillOpacity': 0
     }
     
@@ -83,16 +83,33 @@ with col1:
         'weight': 2.5,
         'fillOpacity': 0.7
     }
+    
+    tooltip = folium.GeoJsonTooltip(
+            fields=['R05大ゾーン', 'CITY_NAME'], 
+            aliases=['大ゾーン', '都市名'],
+            localize=True,
+            sticky=True,
+            labels=True,
+            style="""
+            background-color: #F0EFEF;
+            border: 2px solid black;
+            border-radius: 3px;
+            box-shadow: 3px;
+            """,
+            )
 
-    folium.GeoJson(
+    area_info = folium.GeoJson(
         geo_data,
         name="松山マップ",
         style_function=style_function,
         highlight_function=highlight_function,
-        tooltip=folium.GeoJsonTooltip(fields=['R05大ゾーン', 'CITY_NAME'], aliases=['大ゾーン', '都市名'])
-    ).add_to(m)
+        tooltip=tooltip
+    )
     
-    st_folium(m, height=350, use_container_width=True, returned_objects=[])
+    m.add_child(area_info)
+    m.keep_in_front(area_info)
+    
+    st_folium(m, height=400, use_container_width=True, returned_objects=[])
     
 
 with col2:
@@ -125,104 +142,106 @@ with col2:
     # データ数
     st.write(f"該当するデータ: {len(df2_selected)}人, {len(df3_selected)}回の移動")
 
+# 国勢調査の結果-------------------------------------------------------------------
 with col3:
-    st.subheader('地域の基本情報')
-    st.write('<span style="color:green"> 小数字</span>は都市圏全体の平均値との差を表します。', unsafe_allow_html=True)
-    # 上段の3列構成
-    col3_1, col3_2 = st.columns(2)
-    with col3_1:
-        # 外出率
-        out_rate = (df2_selected[f'{selected_day_text}外出'] * df2_selected['拡大係数']).mean(skipna=True)
-        mean_out_rate = (df2[f'{selected_day_text}外出'] * df2['拡大係数']).mean(skipna=True)
-        st.metric(label="外出した人の割合",
-                value = f"{round(out_rate * 100, 1)} %",
-                delta = f"{round((out_rate - mean_out_rate) * 100, 1)} %")
-        
-        # トリップ数（ネット）
-        df2_net = df2_selected.loc[df2_selected[f'{selected_day_text}トリップ数']>0,:]
-        trip_num = (df2_net[f'{selected_day_text}トリップ数'] * df2_net['拡大係数']).mean(skipna=True)
-        df2_net = df2.loc[df2[f'{selected_day_text}トリップ数']>0,:]
-        mean_trip_num = (df2_net[f'{selected_day_text}トリップ数'] * df2_net['拡大係数']).mean(skipna=True)
-        st.metric(label="移動回数（外出した人）",
-                value=round(trip_num, 2),
-                delta = round(trip_num - mean_trip_num, 2))
+    st.subheader('地域の人口構成')
+    st.write(f'{selected_area}の年代別人口')
     
-    # with col3_2:
-        # 手段
-        # df3_selectedの割合計算
-        # counts_selected = df3_selected.groupby('代表交通手段_概要')['拡大係数'].sum()
-        # total_selected = df3_selected['拡大係数'].sum()
-        # ratio_selected = counts_selected.sort_index() / total_selected
-        # for i in set(['鉄道', '路面電車', 'バス', '自動車', '自転車', '徒歩']) - set(ratio_selected.index):
-        #     ratio_selected[i] = 0
+    if selected_area != '全域':
+        df = pd.read_csv('census_data/population_by_generation.csv',index_col=0)
+        # グラフを描画
+        fig, ax = plt.subplots()
+        ax.bar(df.loc[selected_area,:].index,df.loc[selected_area,:],width=0.6)
+        plt.xticks(rotation=45)
+        st.pyplot(plt)
+        
+    elif selected_area == '全域':
+        PATH = 'census_data/census_population.csv'
+        df_census = pd.read_csv(PATH, encoding='shift_jis',header=4)
 
-        # # df3の割合計算
-        # counts_all = df3.groupby('代表交通手段_概要')['拡大係数'].sum()
-        # total_all = df3['拡大係数'].sum()
-        # ratio_all = counts_all.sort_index() / total_all
+        # 対象の地域のみ抽出
+        df_census = df_census[(df_census['市区町村名'] == '松山市') | (df_census['市区町村名'] == '東温市')|
+                (df_census['市区町村名'] == '伊予市')|(df_census['市区町村名'] == '松前町')|(df_census['市区町村名'] == '砥部町')]
+        df_census = df_census[(df_census['男女'] == '総数')]
+        # 地域階層レベル1に絞る
+        df_census_level1 = df_census[(df_census['地域階層レベル'] == 1)]
+        # 年齢を超細かく見ると...
+        df_census_level1 = df_census_level1[['市区町村コード', '町丁字コード', '地域階層レベル','都道府県名', '市区町村名', '大字・町名', '字・丁目名', '総数',
+                    '年齢「不詳」', '（再掲）15歳未満','（再掲）15〜64歳', '（再掲）65歳以上', '-','-.1']]
+        df_census_level1_st = df_census_level1.loc[: , ["（再掲）15歳未満", "（再掲）15〜64歳",'（再掲）65歳以上','年齢「不詳」']].astype('int')
+        df_census_level1_st=df_census_level1_st.rename(columns={'（再掲）15歳未満': '15歳未満','（再掲）15〜64歳': '15〜64歳','（再掲）65歳以上': '65歳〜','年齢「不詳」': '不明',}, 
+                                                        index={0: '松山市',2576: '伊予市',2871: '東温市',3015: '松前町',3036: '砥部町'})
+        df_sum = df_census_level1_st.sum()
+        # グラフを描画
+        fig, ax = plt.subplots()
+        ax.bar(df_sum.index,df_sum)
+        # Y軸ラベルを「万人」単位で表示
+        # ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f'{int(x/10000)}万人'))
+        st.pyplot(plt)
+# --------------------------------------------------------------------------------
 
-        # st.metric(label="自動車利用率",
-        #         value=f"{round(ratio_selected['自動車'] * 100, 1)} %",
-        #         delta = f"{round((ratio_selected['自動車'] - ratio_all['自動車']) * 100, 1)} %")
-        
-        # # 手段
-        # value = ratio_selected.loc[['鉄道', '路面電車', 'バス']].sum()
-        # mean = ratio_all.loc[['鉄道', '路面電車', 'バス']].sum()
-        # st.metric(label="公共交通利用率",
-        #         value=f"{round(value * 100, 1)} %",
-        #         delta = f"{round((value - mean) * 100, 1)} %")
-        
-        # # 手段
-        # st.metric(label="自転車利用率",
-        #         value=f"{round(ratio_selected['自転車'] * 100, 1)} %",
-        #         delta = f"{round((ratio_selected['自転車'] - ratio_all['自転車']) * 100, 1)} %")
-        
-        # # 手段
-        # st.metric(label="徒歩率",
-        #         value=f"{round(ratio_selected['徒歩'] * 100, 1)} %",
-        #         delta = f"{round((ratio_selected['徒歩'] - ratio_all['徒歩']) * 100, 1)} %")
+
+st.subheader('地域の基本情報')
+st.write('<span style="color:green"> 小数字</span>は都市圏全体の平均値との差を表します。', unsafe_allow_html=True)
+# 上段の3列構成
+col4_1, col4_2, col4_3 = st.columns(3)
+with col4_1:
+    # 外出率
+    out_rate = (df2_selected[f'{selected_day_text}外出'] * df2_selected['拡大係数']).mean(skipna=True)
+    mean_out_rate = (df2[f'{selected_day_text}外出'] * df2['拡大係数']).mean(skipna=True)
+    st.metric(label="外出した人の割合",
+            value = f"{round(out_rate * 100, 1)} %",
+            delta = f"{round((out_rate - mean_out_rate) * 100, 1)} %")
     
-    # 移動時間
-    
-    
-    # 移動距離
+    # トリップ数（ネット）
+    df2_net = df2_selected.loc[df2_selected[f'{selected_day_text}トリップ数']>0,:]
+    trip_num = (df2_net[f'{selected_day_text}トリップ数'] * df2_net['拡大係数']).mean(skipna=True)
+    df2_net = df2.loc[df2[f'{selected_day_text}トリップ数']>0,:]
+    mean_trip_num = (df2_net[f'{selected_day_text}トリップ数'] * df2_net['拡大係数']).mean(skipna=True)
+    st.metric(label="移動回数（外出した人）",
+            value=round(trip_num, 2),
+            delta = round(trip_num - mean_trip_num, 2))
 
-    # 花本担当分 国勢調査の結果-------------------------------------------------------------------
-    with col3:
-        st.write(f'{selected_area}の年代別人口')
-        
-        if selected_area != '全域':
-            df = pd.read_csv('census_data/population_by_generation.csv',index_col=0)
-            # グラフを描画
-            fig, ax = plt.subplots()
-            ax.bar(df.loc[selected_area,:].index,df.loc[selected_area,:],width=0.6)
-            plt.xticks(rotation=45)
-            st.pyplot(plt)
-            
-        elif selected_area == '全域':
-            PATH = 'census_data/census_population.csv'
-            df_census = pd.read_csv(PATH, encoding='shift_jis',header=4)
+with col4_2:
+    # 手段
+    # df3_selectedの割合計算
+    counts_selected = df3_selected.groupby('代表交通手段_概要')['拡大係数'].sum()
+    total_selected = df3_selected['拡大係数'].sum()
+    ratio_selected = counts_selected.sort_index() / total_selected
+    for i in set(['鉄道', '路面電車', 'バス', '自動車', '自転車', '徒歩']) - set(ratio_selected.index):
+        ratio_selected[i] = 0
 
-            # 対象の地域のみ抽出
-            df_census = df_census[(df_census['市区町村名'] == '松山市') | (df_census['市区町村名'] == '東温市')|
-                    (df_census['市区町村名'] == '伊予市')|(df_census['市区町村名'] == '松前町')|(df_census['市区町村名'] == '砥部町')]
-            df_census = df_census[(df_census['男女'] == '総数')]
-            # 地域階層レベル1に絞る
-            df_census_level1 = df_census[(df_census['地域階層レベル'] == 1)]
-            # 年齢を超細かく見ると...
-            df_census_level1 = df_census_level1[['市区町村コード', '町丁字コード', '地域階層レベル','都道府県名', '市区町村名', '大字・町名', '字・丁目名', '総数',
-                        '年齢「不詳」', '（再掲）15歳未満','（再掲）15〜64歳', '（再掲）65歳以上', '-','-.1']]
-            df_census_level1_st = df_census_level1.loc[: , ["（再掲）15歳未満", "（再掲）15〜64歳",'（再掲）65歳以上','年齢「不詳」']].astype('int')
-            df_census_level1_st=df_census_level1_st.rename(columns={'（再掲）15歳未満': '15歳未満','（再掲）15〜64歳': '15〜64歳','（再掲）65歳以上': '65歳〜','年齢「不詳」': '不明',}, 
-          index={0: '松山市',2576: '伊予市',2871: '東温市',3015: '松前町',3036: '砥部町'})
-            df_sum = df_census_level1_st.sum()
-            # グラフを描画
-            fig, ax = plt.subplots()
-            ax.bar(df_sum.index,df_sum)
-            # Y軸ラベルを「万人」単位で表示
-            # ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f'{int(x/10000)}万人'))
-            st.pyplot(plt)
-    # --------------------------------------------------------------------------------
+    # df3の割合計算
+    counts_all = df3.groupby('代表交通手段_概要')['拡大係数'].sum()
+    total_all = df3['拡大係数'].sum()
+    ratio_all = counts_all.sort_index() / total_all
+
+    st.metric(label="自動車利用率",
+            value=f"{round(ratio_selected['自動車'] * 100, 1)} %",
+            delta = f"{round((ratio_selected['自動車'] - ratio_all['自動車']) * 100, 1)} %")
+    
+    # 手段
+    value = ratio_selected.loc[['鉄道', '路面電車', 'バス']].sum()
+    mean = ratio_all.loc[['鉄道', '路面電車', 'バス']].sum()
+    st.metric(label="公共交通利用率",
+            value=f"{round(value * 100, 1)} %",
+            delta = f"{round((value - mean) * 100, 1)} %")
+
+with col4_3:
+    # 手段
+    st.metric(label="自転車利用率",
+            value=f"{round(ratio_selected['自転車'] * 100, 1)} %",
+            delta = f"{round((ratio_selected['自転車'] - ratio_all['自転車']) * 100, 1)} %")
+    
+    # 手段
+    st.metric(label="徒歩率",
+            value=f"{round(ratio_selected['徒歩'] * 100, 1)} %",
+            delta = f"{round((ratio_selected['徒歩'] - ratio_all['徒歩']) * 100, 1)} %")
+
+# 移動時間
+
+
+# 移動距離
 
 
 
